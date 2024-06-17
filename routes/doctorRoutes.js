de9b8091f5e3b0fb93fb32e12ecc49b9596642c9
Randika Dilshan <rdilshan077788@gmail.com>
2024-06-17 13:44:18 +0530

    login route add

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const doctorController = require('../controllers/doctorController');
 
 router.post('/create',doctorController.createDoctor );
+router.post('/login',doctorController.login );
+
 
 module.exports = router;
