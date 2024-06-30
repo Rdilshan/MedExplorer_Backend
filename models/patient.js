447a5ca45ec02b882fef13e56c69027d3edeeca6
Randika Dilshan <rdilshan077788@gmail.com>
2024-06-30 12:10:11 +0530

    patient model create

---
+++ models/patient.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const PatientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    NIC:{
+        type: String,
+        required: true
+    },
+    ProfileIMG:{
+        type: String
+    },
+    PhoneNumber:{
+        type:String
+    },
+    Gender:{
+        type:String
+    }
+});
+
+
+// Pre-save hook to hash the password before saving
+DocotorSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+// Method to compare password
+DocotorSchema.methods.comparePassword = function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+
+
+module.exports = mongoose.model('Patient', PatientSchema);
