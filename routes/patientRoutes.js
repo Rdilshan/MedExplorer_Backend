38e013c1e55331fed272c8842f6a10b990970d4e
Randika Dilshan <rdilshan077788@gmail.com>
2024-07-01 10:30:14 +0530

    add doctor middlware

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const patientController  = require('../controllers/patientController');
 const {patientauthMiddleware} = require("../middlware/patientmiddlware");
+const  {authMiddleware}   = require("../middlware/doctormiddlware")
+
 
 router.post('/create',patientController.createPatient );
-router.get('/get/:nic',patientController.getoneusingnic );
+router.get('/get/:nic',authMiddleware,patientController.getoneusingnic );
 router.post('/login',patientController.login );
 router.get('/profile',patientauthMiddleware,patientController.patientdetails );
 
