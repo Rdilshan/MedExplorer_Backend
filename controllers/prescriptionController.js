fa9581ef3eaddaf349df3a9aab54c8978fb7a467
Randika Dilshan <rdilshan077788@gmail.com>
2024-07-02 17:03:42 +0530

    each doctor write prescription get

--- controllers/prescriptionController.js
+++ controllers/prescriptionController.js
@@ -20,4 +20,18 @@ exports.createPrescription = async (req, res) => {
     }
   };
 
+  exports.getdoctorprescription = async (req, res) => {
+    const userId = req.user.id; 
 
+    try {
+      const prescriptions = await Prescription.find({ doctorid: userId });
+  
+      if (!prescriptions) {
+        return res.status(404).json({ error: 'Prescriptions not found' });
+      }
+  
+      res.status(200).json(prescriptions);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  };
--- routes/prescriptionRotes.js
+++ routes/prescriptionRotes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const prescriptionController  = require('../controllers/prescriptionController');
+const  {authMiddleware}   = require("../middlware/doctormiddlware")
+
 
 
 
 router.post('/create',prescriptionController.createPrescription );
+router.get('/getdoctor',authMiddleware,prescriptionController.getdoctorprescription );
+
 
 module.exports = router;
