b0d56259efb7d3c680a390d1a710d6110f011c90
Randika Dilshan <rdilshan077788@gmail.com>
2024-07-02 19:12:59 +0530

    now okay

--- controllers/prescriptionController.js
+++ controllers/prescriptionController.js
@@ -24,17 +24,17 @@ exports.createPrescription = async (req, res) => {
 
   exports.getdoctorprescription = async (req, res) => {
     const userId = req.user.id; 
-    res.status(200).json(userId);
 
-    // try {
-    //   const prescriptions = await Prescription.find({ doctorid: userId });
+
+    try {
+      const prescriptions = await Prescription.find({ doctorid: userId });
   
-    //   if (!prescriptions) {
-    //     return res.status(404).json({ error: 'Prescriptions not found' });
-    //   }
+      if (!prescriptions) {
+        return res.status(404).json({ error: 'Prescriptions not found' });
+      }
   
-    //   res.status(200).json(prescriptions);
-    // } catch (err) {
-    //   res.status(500).json({ error: err.message });
-    // }
+      res.status(200).json(prescriptions);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
   };
