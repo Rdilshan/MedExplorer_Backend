e54ded602b6b019972f057231d9189a3f7b8d6db
Bimsara <ravindu.bimsara97@gmail.com>
2024-07-24 22:21:00 +0530

    login

--- models/admin.js
+++ models/admin.js
@@ -1,16 +1,11 @@
 const mongoose = require('mongoose');
 
-const adminSchema = new mongoose.schema(
-    {
-        name: {
-            type: String,
-            require: true
-        },
-        password:{
-            type: String,
-            require: true
-        }
-    }
-);
+const adminSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
 
-module.exports = mongoose.modle('Admin', adminSchema);
+});
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+module.exports = Admin;
