1e0fd4642085d82bf70b15d6532178b8f697fa2f
Bimsara <ravindu.bimsara97@gmail.com>
2024-07-25 08:36:56 +0530

    count

--- middlware/adminmiddlware.js
+++ middlware/adminmiddlware.js
@@ -1,22 +1,19 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
-const secret = "your_jwt_secret";
+const adminAuthMiddleware = (req, res, next) => {
+  const token = req.cookies.token || req.headers['authorization'];
 
-exports.adminauthMiddleware = async (req, res, next) => {
-  const bearerHeader = req.headers["authorization"];
+  if (!token) {
+    return res.status(401).json({ error: 'Access denied. No token provided.' });
+  }
 
-  if (typeof bearerHeader !== "undefined") {
-    const bearer = bearerHeader.split(" ");
-    const token = bearer[1];
-
-    jwt.verify(token, secret, (err, decoded) => {
-      if (err) {
-        return res.status(403).json({ error: 'Invalid authorization' });
-      }
-      req.user = decoded;
-      next();
-    });
-  } else {
-    return res.status(403).json({ error: 'Invalid authorization' });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.admin = decoded;
+    next();
+  } catch (ex) {
+    res.status(400).json({ error: 'Invalid token.' });
   }
 };
+
+module.exports = adminAuthMiddleware;
--- models/admin.js
+++ models/admin.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const adminSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-
 });
 
-const Admin = mongoose.model('Admin', adminSchema);
+// Hash the password before saving the admin
+adminSchema.pre('save', async function(next) {
+  if (this.isModified('password') || this.isNew) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
 
+adminSchema.methods.comparePassword = function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+
+
+const Admin = mongoose.model('Admin', adminSchema);
 module.exports = Admin;
