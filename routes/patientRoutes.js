96b3b600f4760ce453fc44cc58221ef61c625060
Randika Dilshan <rdilshan077788@gmail.com>
2024-08-11 07:30:29 +0530

    patient email rout add

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const patientController  = require('../controllers/patientController');
 const {patientauthMiddleware} = require("../middlware/patientmiddlware");
-const  {authMiddleware}   = require("../middlware/doctormiddlware")
+const  {authMiddleware}   = require("../middlware/doctormiddlware");
+
+const emailController = require('../controllers/emailController');
+
 
 
 router.post('/create',patientController.createPatient );
@@ -10,6 +13,9 @@ router.get('/get/:nic',authMiddleware,patientController.getoneusingnic );
 router.post('/login',patientController.login );
 router.get('/profile',patientauthMiddleware,patientController.patientdetails );
 router.post('/editprofile',patientauthMiddleware,patientController.profileEdit);
+
+router.post('/updatepwd', patientController.updatepwd);
+router.post('/otpsend', emailController.patientotpsend);
 
 
 
